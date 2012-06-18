c4d8ba749a1470731d8c26fd3a3dae171fcb7257
Alessio Rocco <alessio.rocco.lt@gmail.com>
2012-06-18 16:42:56 +0200

    Add rspec configuration

--- template.rb
+++ template.rb
@@ -28,3 +28,64 @@ run 'bundle install'
 generate "mongoid:config"
 generate "rspec:install"
 generate "cucumber:install"
+
+empty_directory "spec/support"
+empty_directory "spec/factories"
+remove_file "spec/spec_helper.rb"
+create_file "spec/spec_helper.rb" do
+  <<RUBY
+require 'rubygems'
+require 'spork'
+#uncomment the following line to use spork with the debugger
+#require 'spork/ext/ruby-debug'
+
+Spork.prefork do
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
+  require 'rspec/autorun'
+
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+  RSpec.configure do |config|
+    config.infer_base_class_for_anonymous_controllers = false
+
+    config.treat_symbols_as_metadata_keys_with_true_values = true
+    config.filter_run :focus => true
+    config.run_all_when_everything_filtered = true
+  end
+
+end
+
+Spork.each_run do
+  FactoryGirl.reload
+end
+RUBY
+end
+
+create_file "spec/support/database_cleaner.rb" do
+  <<RUBY
+RSpec.configure do |config|
+  # Clean/Reset Mongoid DB prior to running the tests
+  config.before :each do
+    Mongoid.master.collections.select {|c| c.name !~ /system/ }.each(&:drop)
+  end
+end
+RUBY
+end
+
+create_file "spec/support/factory_girl.rb" do
+  <<RUBY
+RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+end
+RUBY
+end
+
+create_file "spec/support/mongoid.rb" do
+  <<RUBY
+RSpec.configure do |config|
+  config.include Mongoid::Matchers
+end
+RUBY
+end
